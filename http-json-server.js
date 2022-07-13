759a72259428c77168c4d2ed7e45ddf6997cf907
FilipDolejsi <sonofmydad@live.com>
2022-07-13 16:40:52 +0200

    Evaluation update

--- http-json-server.js
+++ http-json-server.js
@@ -5,13 +5,19 @@ import vscodeUri from 'vscode-uri'
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import express from 'express';
 
+var app = express();
 var valLocation = path.join("val_binaries", "val.json");
 var valBinariesText = fs.readFileSync(valLocation, { encoding: "utf-8" });
 var parserExe = JSON.parse(valBinariesText).parserPath;
 var parserPath = path.join("val_binaries", parserExe);
+
+var valStepExe = JSON.parse(valBinariesText).valStepPath;
+var valStepDir = path.join("val_binaries", valStepExe);
 console.log(parserPath);
-var server = http.createServer(function (request, response) {
+
+app.post('/parse', function (request, response) {
     var allData = "";
     request.on('data', async function (data) {
         allData += data;
@@ -62,12 +68,59 @@ var server = http.createServer(function (request, response) {
     });
 });
 
-console.log('Service is listening at :8085');
-server.listen(8085, 'localhost');
 
-server.on('connection', (stream) => {
-    console.log('someone connected!');
-});
+app.post('/evaluation', async function (request, response) {
+    console.log("Got a POST request for the homepage");
+    var allData = "";
+    request.on('data', async function (data) {
+        allData += data;
+    });
+
+    request.on('end', async function () {
+        var info = JSON.parse(allData.toString());
+        var domainText = info.domain;
+        const domain = parser.PddlDomainParser.parseText(domainText);
+        var problemText = info.problem;
+        const problem = await parser.PddlProblemParser.parseText(problemText);
+        var planText = info.plan;
+        const plan = new parser.PddlPlanParser().parseText(planText, 0.001);
+        const allHappenings = plan.getHappenings();
+
+        const valStep = new ValStep(domain, problem);
+        try {
+    
+            var valuesAtEnd = await valStep.executeBatch(allHappenings, {
+                valStepPath: valStepDir,
+                verbose: false
+            })
+
+            // check for undefined; return some failure state
+
+            console.log(`Values at end: ` +
+                valuesAtEnd
+                    .map(v => `${v.getVariableName()}=${v.getValue()}`)
+                    .join(', '));
+        
+            response.send(`Values at end: ` +
+            valuesAtEnd
+                .map(v => `${v.getVariableName()}=${v.getValue()}`)
+                .join(', '));
+        
+        } catch (error) {
+            console.error(error);
+            response.send(error.message);
+        }
+    
+    })
+})
+
+ var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
+
 function toProblem(issue, fileUri) {
     var fileType;
     if (fileUri.includes('domain')){
@@ -90,4 +143,3 @@ function toProblem(issue, fileUri) {
         "message": problem
     };
 }
-
